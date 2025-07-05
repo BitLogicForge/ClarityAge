c94be3c1f164d2125c6798fa56a5e197a3f48608
SuperAtrapa <superatrapa@gmail.com>
2025-07-05 02:07:58 +0200

    Add philosophies section and corresponding questions to localization files

---
+++ src/config/questions.ts
@@ -0,0 +1,16 @@
+export const questions = [
+  {
+    title: "philosophies.q1.title",
+    description: "philosophies.q1.description",
+    quote: "philosophies.q1.quote",
+    author: "philosophies.q1.author",
+    question: "philosophies.q1.question",
+  },
+  {
+    title: "philosophies.q2.title",
+    description: "philosophies.q2.description",
+    quote: "philosophies.q2.quote",
+    author: "philosophies.q2.author",
+    question: "philosophies.q2.question",
+  },
+];
