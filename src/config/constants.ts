882f5d6b9da7087ce46679190a4484b4c6f834db
SuperAtrapa <superatrapa@gmail.com>
2025-07-05 19:01:35 +0200

    Update project metadata and structure: correct HTML doctype, update homepage URL, and configure Vite with APP_NAME constant

---
+++ src/config/constants.ts
@@ -0,0 +1,5 @@
+export const APP_NAME = "ClarityAge";
+export const APP_DESCRIPTION =
+  "A React app to explore philosophical questions and personal values through a Likert scale.";
+export const APP_VERSION = "0.0.1";
+export const APP_AUTHOR = "BitLogicForge";
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { APP_NAME } from "./src/config/constants";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: APP_NAME ? `/${APP_NAME}/` : "/",
   plugins: [react()],
-})
+});
